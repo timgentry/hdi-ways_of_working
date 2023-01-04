cabfb3eb26a3ee52033e8f1b16218b2e6cd0c9ac
Tim Gentry <52189+timgentry@users.noreply.github.com>
2023-01-04 13:35:11 +0000

    Initial commit of the linter generator

---
+++ lib/generators/hdi/ways_of_working/linter/install/install_generator.rb
@@ -0,0 +1,55 @@
+module Hdi
+  module WaysOfWorking
+    module Linter
+      # This generator add the MADR files to the doc/decisions folder
+      class InstallGenerator < Rails::Generators::Base
+        source_root File.expand_path("templates", __dir__)
+
+        # TODO: copy_rubocop_github_workflow_action
+
+        def copy_megalinter_github_workflow_action
+          copy_file '.github/workflows/mega-linter.yml'
+        end
+
+        def copy_megalinter_dot_file
+          copy_file '.mega-linter.yml'
+        end
+
+        def copy_prettierignore_dot_file
+          copy_file '.prettierignore'
+        end
+
+        def copy_yamllint_dot_file
+          copy_file '.yamllint.yml'
+        end
+
+        def create_gitignore_if_missing
+          create_file_if_missing '.gitignore'
+        end
+
+        def gitignore_reports_folder
+          append_to_file '.gitignore', "megalinter-reports/\n"
+        end
+
+        def gitignore_rubocop_cached_file
+          append_to_file '.gitignore', ".rubocop-https---raw-githubusercontent-com-NHSDigital-ndr-dev-support-v6-1-9-config-rubocop-ndr-yml\n"
+        end
+
+        # TODO: add hdi/ways_of_working/tasks to Rakefile
+        # inject_into_file 'Rakefile', after: "require_relative "config/application"\n" do <<-'RUBY'
+        #   require 'hdi/ways_of_working/tasks' if Rails.env.development? || Rails.env.test?
+        # RUBY
+        # end
+
+        private
+
+        def create_file_if_missing(path)
+          path = File.join(destination_root, path)
+          return if behavior == :revoke || File.exist?(path)
+
+          File.open(path, 'w') { |file| file.write }
+        end
+      end
+    end
+  end
+end
---
+++ test/lib/generators/hdi/ways_of_working/linter/install_generator_test.rb
@@ -0,0 +1,40 @@
+require "test_helper"
+require "generators/hdi/ways_of_working/linter/install/install_generator"
+
+class Hdi::WaysOfWorking::Linter::InstallGeneratorTest < Rails::Generators::TestCase
+  tests Hdi::WaysOfWorking::Linter::InstallGenerator
+  destination Rails.root.join("tmp/generators")
+  setup :prepare_destination
+
+  test 'generator runs without errors' do
+    assert_nothing_raised do
+      run_generator
+    end
+  end
+
+  test 'files are created and revoked' do
+    run_generator
+
+    assert_file '.github/workflows/mega-linter.yml'
+    assert_file '.mega-linter.yml'
+    assert_file '.prettierignore'
+    assert_file '.yamllint.yml'
+
+    assert_file '.gitignore' do |content|
+      assert_match("megalinter-reports/\n", content)
+      assert_match(".rubocop-https---raw-githubusercontent-com-NHSDigital-ndr-dev-support-v6-1-9-config-rubocop-ndr-yml\n", content)
+    end
+
+    run_generator [], behavior: :revoke
+
+    assert_no_file '.github/workflows/mega-linter.yml'
+    assert_no_file '.mega-linter.yml'
+    assert_no_file '.prettierignore'
+    assert_no_file '.yamllint.yml'
+
+    assert_file '.gitignore' do |content|
+      refute_match("megalinter-reports/\n", content)
+      refute_match(".rubocop-https---raw-githubusercontent-com-NHSDigital-ndr-dev-support-v6-1-9-config-rubocop-ndr-yml\n", content)
+    end
+  end
+end
